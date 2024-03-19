5aed286153bcbf91878b38b242688947e5672f05
ahmedfarag0011 <163968879+ahmedfarag0011@users.noreply.github.com>
2024-03-19 19:03:20 +0200

    Update variable.tf

--- terraform_project/modules/security_group/variable.tf
+++ terraform_project/modules/security_group/variable.tf
@@ -4,3 +4,6 @@ description = "its the CIDR for ingress security_group"
 variable "outbound" {
 description = "its the CIDR for egress security_group"
 }
+variable "vpc_varaiable" {
+description = "VPC ID"
+}
