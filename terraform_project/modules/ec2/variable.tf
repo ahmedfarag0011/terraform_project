9172f76efaf58b5983cd905103b44cdf61a16355
ahmedfarag0011 <163968879+ahmedfarag0011@users.noreply.github.com>
2024-03-19 19:01:04 +0200

    Update variable.tf

--- terraform_project/modules/ec2/variable.tf
+++ terraform_project/modules/ec2/variable.tf
@@ -7,3 +7,8 @@ variable "instance_type" {
     description= "its the instance_type of ec2 instance"
     type= string 
 }
+
+variable "public_subnet" {
+    description= "its the public subnet id"
+    type=string
+}
